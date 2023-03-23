d3b75f7d8e2df43a306bb83253e518418462211c
NhanVo <nhanvt.hcm@gmail.com>
2023-03-23 18:56:43 +0700

    update hangman program

--- hangman.py
+++ hangman.py
@@ -8,7 +8,7 @@ def get_valid_word(words):
     while "-" in word or " " in words:
         word = random.choice(words)
 
-    return word
+    return word.upper()
 
 
 def hangman():
@@ -16,26 +16,44 @@ def hangman():
     word_letters = set(word)  # letter in the word
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # what the user has guessed
+    lives = 7
 
     # getting user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
+
         # letters used
         # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-        print("You have used these letters: ", " ".join(used_letters))
+        print(
+            "You have ",
+            lives,
+            "lives left and you have used these letters: ",
+            " ".join(used_letters),
+        )
+
+        # What current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else "-" for letter in word]
+        print("Current word: ", " ".join(word_list))
 
         user_letter = input("Guess a letter: ").upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+                print("")
+            else:
+                lives -= 1  # take away a live is it's wrong
+                print("\nYour letter", user_letter, "is not in the word")
         elif user_letter in used_letters:
             print("you have already used that character. Please try again.")
         else:
             print("Invalid character")
 
     # gets here when len(word_letters) == 0
+    if lives == 0:
+        print("You died, sorry, the word is: ", word)
+    else:
+        print("Yay! You guessed the word ", word, "!!")
 
 
-# user_input = input('Type something: ')
-# print(user_input)
-hangman()
+if __name__ == "__main__":
+    hangman()
