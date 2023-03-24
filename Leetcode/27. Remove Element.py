ddba0ea91779c0a2d762aee60446de16f5603169
NhanVo <nhanvt.hcm@gmail.com>
2023-03-24 16:35:26 +0700

    add Leetcode exercise

---
+++ Leetcode/27. Remove Element.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # count for keeping track of elements other than val
+        count = 0
+        # loop through all the elements of the array
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[count] = nums[i]
+                count += 1
+        return count
+
+
+# Testing
+tmp = Solution()
+a = [3, 2, 2, 3]
+
+print(tmp.removeElement(a, 2))
